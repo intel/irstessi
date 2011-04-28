18a86c57773d189d3d9586a20c8765d0339ddd0b
Anna Czarnowska <anna.czarnowska@intel.com>
2011-04-28 13:16:26 +0200

    inst.sh: run ut_session after install
    Signed-off-by: Anna Czarnowska <anna.czarnowska@intel.com>

--- inst.sh
+++ inst.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -ex
+origdir=`pwd`
+cd /root/SSI_API
 ./autogen.sh --libdir='${prefix}/lib64'
 CXXFLAGS=-g3 ./configure --libdir='${prefix}/lib64'
 make
 make install
-g++ -o ania ut_ania.cpp -g3 -L/usr/local/lib64 -lssi 
-echo aaa > /tmp/my.log
-./ania
-cat /tmp/my.log
+#g++ -o ania ut_ania.cpp -g3 -L/usr/local/lib64 -lssi 
+#echo aaa > /tmp/my.log
+#./ania
+#cat /tmp/my.log
+cd ut
+./ut_session
+cd $origdir
