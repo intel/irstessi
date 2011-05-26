384847bfdb3a2a0b63565a45969f0facf12dd55d
Anna Czarnowska <anna.czarnowska@intel.com>
2011-05-26 16:16:04 +0200

    inst.sh script modified
    Signed-off-by: Anna Czarnowska <anna.czarnowska@intel.com>

--- inst.sh
+++ inst.sh
@@ -1,14 +1,8 @@
 #!/bin/bash -ex
-origdir=`pwd`
-cd /root/SSI_API
+cd `pwd`/`dirname $0`
 ./autogen.sh --libdir='${prefix}/lib64'
 CXXFLAGS=-g3 ./configure --libdir='${prefix}/lib64'
 make
 make install
-#g++ -o ania ut_ania.cpp -g3 -L/usr/local/lib64 -lssi 
-#echo aaa > /tmp/my.log
-#./ania
-#cat /tmp/my.log
 cd ut
 ./ut_session
-cd $origdir
