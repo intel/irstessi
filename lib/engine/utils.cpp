8aaaaeceaaf7ba622349f84974842425fd48f611
Anna Czarnowska <anna.czarnowska@intel.com>
2011-09-13 15:02:30 +0200

    Run command passed to 'shell' function in a subprocess.
    shell command runs indirectly popen and ignores output.
    This patch changes it so that shell command run a subprocess
    which closes all opened file descriptors and then using
    execl invokes given command.
    
    Signed-off-by: Maciej Patelczyk <maciej.patelczyk@intel.com>

--- lib/engine/utils.cpp
+++ lib/engine/utils.cpp
@@ -21,6 +21,13 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 
 #include <features.h>
 #include <cstdio>
+#include <sys/wait.h>
+#include <errno.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include "exception.h"
 #include "list.h"
@@ -47,11 +54,79 @@ int shell_cap(const String &s, String &r)
     return pclose(pd);
 }
 
-/* */
+
+static void close_parent_fds(void)
+{
+    DIR *dirp;
+    struct dirent *d_entry;
+    int fd, p_fd;
+
+    dirp = opendir("/proc/self/fd");
+    if (!dirp)
+        return;
+
+    p_fd = dirfd(dirp);
+    if (p_fd == -1) {
+        closedir(dirp);
+        return;
+    }
+
+    while((d_entry = readdir(dirp)) != NULL) {
+        if (!strcmp(d_entry->d_name, ".") ||
+            !strcmp(d_entry->d_name, ".."))
+            continue;
+        errno = 0;
+        fd = (int)strtol(d_entry->d_name, NULL, 10);
+        if (errno)
+            continue;
+        if (p_fd == fd)
+            continue;
+        if (fd > 2)
+            close(fd);
+    }
+    closedir(dirp);
+}
+
+/*
+ * This function launch a shell process that executes given 's' command.
+ *
+ * Note:
+ * This function make fork and in child process it closes all opened
+ * file descriptors before execl is called.
+ * It waits for child process to end.
+ *
+ * Returns:
+ * 0 - success
+ * error otherwise.
+ * */
 int shell(const String &s)
 {
-    String buffer;
-    return shell_cap("export MDADM_EXPERIMENTAL=1; " + s, buffer);
+    pid_t pid;
+    int ret = 0, status=0;
+    String cmd = "export MDADM_EXPERIMENTAL=1; " + s;
+
+    switch(fork()) {
+    case 0:
+        /* Child process.
+         * Before switching into new executable close all non standard
+         * file handlers.*/
+        close_parent_fds();
+        execl("/bin/sh", "sh", "-c", cmd.get(), NULL);
+        /* If we're here then execl failed*/
+        exit(-1);
+        break;
+    case -1:
+        /* Error. Fork failed. */
+        ret = -1;
+        break;
+    default:
+        /* Parent process. */
+        pid = wait(&status);
+        if (pid < 0 || status)
+            ret = -1;
+        break;
+    }
+    return ret;
 }
 
 /* ex: set tabstop=4 softtabstop=4 shiftwidth=4 textwidth=98 expandtab: */
