756da3b857e1abb67ed45489fc1974c87bfa32bc
Anna Czarnowska <anna.czarnowska@intel.com>
2011-11-17 11:15:35 +0100

    ssi.h : routing device component rev length expanded to 5 characters
    Signed-off-by: Anna Czarnowska <anna.czarnowska@intel.com>

--- include/ssi.h
+++ include/ssi.h
@@ -170,7 +170,7 @@ static const SSI_Uint32 SSI_INVALID_SLOTNUMBER = 0xFFFFFFFF;
  *
  * @brief   Ssi routing device component reverse length.
 **/
-#define SSI_ROUTING_DEVICE_COMPONENT_REV_LENGTH 2
+#define SSI_ROUTING_DEVICE_COMPONENT_REV_LENGTH 5
 
 /**
  * @def SSI_ARRAY_NAME_LENGTH
